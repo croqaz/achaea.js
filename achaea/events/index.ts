5ebec24162ad61a0735c3ceb55f02abe35a9559b
Cristi Constantin <git@cristi.email>
2023-10-19 20:30:39 +0100

    New faster events

---
+++ achaea/events/index.ts
@@ -0,0 +1,7 @@
+/*
+ * High-performance EventEmitter implementation
+ * https://github.com/foxifyjs/events
+ * By Ardalan Amini, copyright (c) 2019-2022
+ */
+import { EventEmitter } from './Events.ts';
+export default new EventEmitter();
