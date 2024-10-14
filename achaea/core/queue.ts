f75c31c8cf5b515f4f1a6fb7cd139fbd29906f05
Cristi Constantin <git@cristi.email>
2024-10-14 18:36:58 +0100

    Small tweak to the queues

--- achaea/core/queue.ts
+++ achaea/core/queue.ts
@@ -1,10 +1,12 @@
 import ee from '../events/index.ts';
 import { STATE } from '../core/state.ts';
 
-// Example:
-// queueCmd('bal', 'DO SOMETHING ON BALANCE')
-// queueCmd('eq', 'DO SOMETHING ON EQUILIBRIUM')
-// queueCmd('eb', 'DO SOMETHING WITH BAL & EQ')
+/*
+ * Example:
+ * queueCmd('bal', 'DO SOMETHING ON BALANCE')
+ * queueCmd('eq', 'DO SOMETHING ON EQUILIBRIUM')
+ * queueCmd('eb', 'DO SOMETHING WITH BAL & EQ')
+ */
 export function queueCmd(name: string, cmd: string) {
   if (STATE.Queue[name] && typeof STATE.Me[name] === 'boolean') {
     STATE.Queue[name].push(cmd);
@@ -14,14 +16,15 @@ export function queueCmd(name: string, cmd: string) {
 }
 
 let intID = setInterval(function () {
-  // Consume from all the queues
+  // Consume from all the queues, in order
   for (const [name, queue] of Object.entries(STATE.Queue)) {
     if (queue.length && STATE.Me[name] === true) {
       STATE.Me[name] = false;
       ee.emit('user:text', queue.shift());
+      continue;
     }
   }
-}, 25);
+}, 100);
 
 // Make sure to shut down cleanly
 ee.on('will:quit', function () {
